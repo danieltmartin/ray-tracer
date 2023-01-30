51d7718b5b03fbe4a3589277051888d73db15ee5
Daniel Martin <daniel.t.martin@protonmail.com>
2023-01-30 00:31:40 -0500

    update example output

--- exercises/worldcamera/worldcamera.go
+++ exercises/worldcamera/worldcamera.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"flag"
+	"log"
 	"math"
 	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/danieltmartin/ray-tracer/camera"
 	"github.com/danieltmartin/ray-tracer/floatcolor"
@@ -15,12 +20,29 @@ import (
 	"github.com/danieltmartin/ray-tracer/world"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	floor := primitive.NewPlane()
 	floor.SetMaterial(floor.Material().
 		WithPattern(material.NewCheckerPattern(
 			floatcolor.NewFromInt(0xded3d3), floatcolor.NewFromInt(0x87a9cc))).
 		WithSpecular(0).
+		WithAmbient(0.5).
 		WithReflective(0.2),
 	)
 
@@ -40,39 +62,44 @@ func main() {
 
 	middleSphere := primitive.NewSphere()
 	middleSphere.SetTransform(transform.Identity().RotationX(math.Pi).Translation(-0.5, 1, 0.5).Matrix())
-	middleSphere.SetMaterial(floor.Material().
+	middleSphere.SetMaterial(material.Default.
 		WithPattern(material.NewGradientPattern(
 			floatcolor.NewFromInt(0x9f45FF), floatcolor.NewFromInt(0x05020D)).
 			WithTransform(transform.RotationY(math.Pi / 8))).
 		WithDiffuse(0.7).
-		WithSpecular(0.3))
+		WithReflective(0.2).
+		WithSpecular(0.8))
 
 	rightSphere := primitive.NewSphere()
 	rightSphere.SetTransform(transform.Identity().
 		Scaling(0.5, 0.5, 0.5).
 		Translation(1.5, 0.5, -0.5).
 		Matrix())
-	rightSphere.SetMaterial(floor.Material().
+	rightSphere.SetMaterial(material.Default.
 		WithPattern(material.NewGradientPattern(
-			floatcolor.NewFromInt(0x9f45FF), floatcolor.NewFromInt(0x05020D)).
+			floatcolor.NewFromInt(0xC0451A), floatcolor.NewFromInt(0xF0B7B7)).
 			WithTransform(transform.RotationY(math.Pi / 8))).
 		WithDiffuse(0.7).
-		WithSpecular(0.3))
+		WithReflective(0.2).
+		WithSpecular(0.8))
 
 	leftSphere := primitive.NewSphere()
 	leftSphere.SetTransform(transform.Identity().
 		Scaling(0.33, 0.33, 0.33).
 		Translation(-1.5, 0.33, -0.75).
 		Matrix())
-	leftSphere.SetMaterial(floor.Material().
-		WithPattern(material.NewRingPattern(floatcolor.NewFromInt(0x1f005c), floatcolor.NewFromInt(0xffb56b)).WithTransform(transform.Scaling(0.1, 0.1, 0.1))).
+	leftSphere.SetMaterial(material.Default.
+		WithPattern(material.NewGradientPattern(
+			floatcolor.NewFromInt(0x051937), floatcolor.NewFromInt(0xA8EB12)).
+			WithTransform(transform.RotationY(math.Pi / 8))).
 		WithDiffuse(0.7).
-		WithSpecular(0.3))
+		WithReflective(0.2).
+		WithSpecular(0.8))
 
 	light := light.NewPointLight(tuple.NewPoint(-10, 10, -10), floatcolor.White)
 
 	world := world.New()
-	world.AddPrimitives(&floor, &leftWall, &rightWall, &middleSphere, &leftSphere, &rightSphere)
+	world.AddPrimitives(&floor, &middleSphere, &leftSphere, &rightSphere)
 	world.AddLights(&light)
 
 	camera := camera.New(1920, 1080, math.Pi/3)
@@ -84,9 +111,26 @@ func main() {
 
 	image := camera.Render(world)
 
-	f, err := os.Create("worldcamera2.ppm")
+	f, err := os.Create("worldcamera.ppm")
 	if err != nil {
 		panic(err)
 	}
-	ppm.Encode(f, image)
+	w := bufio.NewWriter(f)
+	ppm.Encode(w, image)
+	err = w.Flush()
+	if err != nil {
+		panic(err)
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close()
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.Lookup("allocs").WriteTo(f, 0); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
 }
